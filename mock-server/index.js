a64e6a5c5fd48247c1b80201788000b2de31c57f
Mustafa <mustafa_dur@hotmail.com>
2024-05-25 02:08:49 +0300

    Implemented missing endpoints to load the UI

--- mock-server/index.js
+++ mock-server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const nocache = require('nocache');
 
 const app = express();
 const port = 3000;
@@ -7,8 +8,27 @@ const port = 3000;
 // Use morgan to log requests to the console
 app.use(morgan('combined'));
 
-// Middleware to parse JSON bodies
+// Don't use cache
+app.use(nocache());
 app.use(express.json());
+
+app.patch('/v3.3/applicants/:id/location', (req, res) => {
+  console.log('Updating applicants for App Id:', req.params.id);
+  const response = {};
+  res.json(response);
+});
+
+app.get('/v3.3/applicants/:id/consents', (req, res) => {
+  console.log('Consent for App Id:', req.params.id);
+  const response = [
+    {
+      'granted': true,
+      'name': 'privacy_notices_read',
+      'required': true
+    }
+  ]
+  res.json(response);
+});
 
 app.post('/v3.5/sdk/configurations', (req, res) => {
   const response = {
@@ -24,14 +44,14 @@ app.post('/v3.5/sdk/configurations', (req, res) => {
 
 // Handle all routes and methods
 app.all('*', (req, res) => {
-    console.log('Headers:', req.headers);
-    if (req.body) {
-        console.log('Body:', req.body);
-    }
-    res.status(200).send('OK');
+  console.log('Headers:', req.headers);
+  if (req.body) {
+    console.log('Body:', req.body);
+  }
+  res.status(200).send('OK');
 });
 
 app.listen(port, () => {
-    console.log(`Mock server is listening on port ${port}`);
+  console.log(`Mock server is listening on port ${port}`);
 });
 
